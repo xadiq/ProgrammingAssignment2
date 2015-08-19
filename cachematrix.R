d86313e00c0f370683158c5aa1ac5a35c49e5308
SADIQ MOHAMMED <xadiq.mohammed@gmail.com>
2015-08-20 01:00:02 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below function calculates invertibleof square matrix
 
-## Write a short comment describing this function
+## Returns list of functions
 
 makeCacheMatrix <- function(x = matrix()) {
+  iMatrix <- NULL
+  
+setMatrixData <- function(inputMatrix){
+  x <- inputMatrix
+  iMatrix <<- NULL
+}
 
+getMatrixData <- function() x
+
+setInvert <- function(invert) iMatrix <<- invert
+
+getInvert <- function() iMatrix
+
+list(setMatrixData = setMatrixData, getMatrixData = getMatrixData, setInvert = setInvert, getInvert= getInvert )
 }
 
 
-## Write a short comment describing this function
+## calculate list 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  iMatrix <- x$getInvert()
+  if(!is.null(iMatrix)){
+    
+    message("getting cashed data")
+    return(iMatrix)
+  }
+  
+  data <- x$getMatrixData()
+  iMatrix <- solve(data)
+  x$setInvert(iMatrix) 
+iMatrix
 }
